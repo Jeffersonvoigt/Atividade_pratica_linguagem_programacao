fc8c791612772e15f927a04b020eec9d9253a533
Jefferson Voigt <Jefferson.voigt@hotmail.com>
2026-06-22 02:11:31 -0300

    initial commit

---
+++ game/main.py
@@ -0,0 +1,15 @@
+
+import pygame
+
+print('setup Start')
+pygame.init()
+window = pygame.display.set_mode(size=(640, 480))
+print('Setup End')
+
+print('Loop Start')
+while True:
+    # check for all events (verificar todos os eventos)
+    for event in pygame.event.get():
+        if event.type  == pygame.QUIT:
+            pygame.quit() # Close window (fechar janela)
+            quit() # end pygame (fim)
